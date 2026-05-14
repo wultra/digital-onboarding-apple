c6f7d429d3bef7aefc1e4e3a1071105fb1ca2908
Petr Dvořák <petr@wultra.com>
2026-05-14 23:35:42 +0200

    Fix #71: Fix typo in 'Document ORC' to 'Document OCR' (#72)

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,14 +5,15 @@ set -u # stop when undefined variable is used
 #set -x # print all execution (good for debugging)
 
 SCRIPT_FOLDER=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+XCODE_PROJECT="WultraDigitalOnboarding.xcodeproj"
+XCODE_SCHEME="WultraDigitalOnboardingTests"
+BUILD_FOLDER="build"
 
-# find latest iOS SDK available
-IOS_VERSION=$(xcrun simctl list | grep "\-\- iOS" | tail -1 | tr -d - | tr -d " " | tr -d "iOS")
-# find the first simulator for this sdk
-SIMULATOR=$(xcrun simctl list | grep "\-\- iOS ${IOS_VERSION} \-\-" -A 1 | tail -1 | sed -E 's/^[[:space:]]+//; s/\(.*//; s/[[:space:]]+$//')
-DESTINATION="platform=iOS Simulator,OS=${IOS_VERSION},name=${SIMULATOR}"
-
-echo "Default destination: ${DESTINATION}"
+# Function that resolves the best available simulator for the test run
+function getSimulatorDestination {
+  local scriptUrl="https://raw.githubusercontent.com/wultra/wultra-infrastructure/refs/heads/mobile/mobile/utils/ios-get-simulator/v1/get-ios-sim.js"
+  curl -fsSL "${scriptUrl}" | node - -p "${SCRIPT_FOLDER}/.." "${XCODE_PROJECT}" "${XCODE_SCHEME}"
+}
 
 CONFIG_JSON=""
 
@@ -20,12 +21,6 @@ CONFIG_JSON=""
 while [[ $# -gt 0 ]]
 do
 	case "$1" in
-		-destination)
-			DESTINATION="$2"
-			echo "Destination obtained as parameter: ${DESTINATION}"
-			shift
-			shift
-			;;
 		-config)
 			CONFIG_JSON="$2"
 			shift
@@ -38,13 +33,17 @@ do
 	esac
 done
 
+# Resolve the newest available iOS Simulator destination through the shared Node helper.
+echo "Resolving the best simulator for the ${XCODE_SCHEME}..."
+DESTINATION=$(getSimulatorDestination)
+
+echo "Simulator to use: ${DESTINATION}"
+
 pushd "${SCRIPT_FOLDER}"
 sh cart-update.sh
 popd
 
 pushd "${SCRIPT_FOLDER}/.."
-
-BUILD_FOLDER="build"
 
 rm -rf "${BUILD_FOLDER}" # clear build folder
 
@@ -68,10 +67,12 @@ printLogs() {
 # make sure that we search for log files even on exit
 trap printLogs EXIT
 
+echo "Starting the test"
+
 xcrun xcodebuild \
 	-derivedDataPath "${BUILD_FOLDER}" \
-  -project "WultraDigitalOnboarding.xcodeproj" \
-  -scheme "WultraDigitalOnboardingTests" \
+  -project "${XCODE_PROJECT}" \
+  -scheme "${XCODE_SCHEME}" \
   -destination "${DESTINATION}" \
   -configuration "Debug" \
   test
